ca466221f7134f621495502decc2458c457a31f7
Puran Singh Rana <puransinghrana080@gmail.com>
2025-07-13 16:20:49 +0530

    more test cases

--- src/components/Expenses.test.js
+++ src/components/Expenses.test.js
@@ -1,16 +1,46 @@
 import { render, screen } from "@testing-library/react";
 import Expenses from "./Expenses";
-import configureStore from "redux-mock-store";
 import { Provider } from "react-redux";
 import store from "../store/index";
 
-test("renders 'All Expenses' heading", () => {
-  render(
-    <Provider store={store}>
-      <Expenses />
-    </Provider>
-  );
 
-  const heading = screen.getByText("All Expenses");
-  expect(heading).toBeInTheDocument();
-});
+describe("Expenses Component",()=>{
+
+  test("renders 'All Expenses' heading", () => {
+    render(
+      <Provider store={store}>
+        <Expenses />
+      </Provider>
+    );
+  
+    const heading = screen.getByText("All Expenses");
+    expect(heading).toBeInTheDocument();
+  });
+  
+  test("render expenses if req success", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      ok:true,
+      json: async () => ({
+  
+        exp1: {
+          amount: '200',
+          description: "Test expense",
+          category: "Food",
+        },
+      }),
+    });
+  
+    render(
+      <Provider store={store}>
+        <Expenses />
+      </Provider>
+    );
+    const amountText = await screen.findByText("200",{exact:false});
+    expect(amountText).toBeInTheDocument()
+  });
+
+
+})
+
+
